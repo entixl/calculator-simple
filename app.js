79421576405035bccce2144ed8181e9ebc9233c1
ential <xdnotavailable@gmail.com>
2025-12-08 21:48:09 +0530

    update js

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-const display = document.getElementById("display");
-function buttonClicked(input) {
-	console.log(input);
-	if (input == "AC") {
-		display.value = "";
-	} else if (input == "DE") {
-		display.value = display.value.slice(0, -1);
-	} else {
-		if (display.value == "Error" || display.value == "Click buttons") {
-			display.value = input;
-		} else {
-			display.value += input;
-		}
+const previousDisplay = document.getElementById("previous");
+const currentDisplay = document.getElementById("current");
+const operatorList = "+-/*";
+let operator;
+function buttonClicked(value) {
+	if (operatorList.includes(value)) {
+		operator = value;
 	}
 }
-function calculate() {
-	console.log("guyg");
-	try {
-		console.log("gumguufyg");
-		display.value = eval(display.value);
-	} catch (e) {
-		console.log(e);
-		display.value = "Error";
-	}
+function allClear() {
+	previousDisplay.value = "";
+	currentDisplay.value = "";
 }
+function deleteEntry() {}
+function calculate() {}
